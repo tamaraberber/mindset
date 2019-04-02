b85078bb49fc4ab052ac67dd82ec42bed1a703b6
tamber <berbertamara@gmail.com>
2019-04-02 18:41:06 +0100

    updates

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ $(function () {
 })
 
 //dismiss popovers
+$('body').on('click', '.filmImg', function() {
+  console.log('hello')
+})
 
 $('html').on('click', function(e) {
   if (typeof $(e.target).data('original-title') == 'undefined' &&
@@ -58,26 +61,86 @@ $(document).ready(function() {
 // }
 //
 
-
 // localStorage.clear()
 
-
+//DOG BOOK TITLES//
 $(function () {
-  $('body').on('click', '.titlesImg', function() {
-    var thing = $('textarea').val();
-    var currentThings = JSON.parse(window.localStorage.getItem('titles')) || []
-    currentThings.push(thing)
-    window.localStorage.setItem('titles', JSON.stringify(currentThings));
-    $('input').val('');
-  })
+  $('body').on('click', '.dogImg', function() {
+    var thing = $('#dogt').val();
+    var currentThings = JSON.parse(window.localStorage.getItem('dog')) || [];
+    currentThings.push(thing);
+    window.localStorage.setItem('dog', JSON.stringify(currentThings));
+    $('#dogt').val('');
+  });
 
+  //ACTIVITY//
+  // quotationsImg, getItem and setItem,
+  $('body').on('click', '.activityImg', function() {
+    var thing = $('#actt').val();
+    var currentThings = JSON.parse(window.localStorage.getItem('activity')) || []
+    currentThings.push(thing)
+    window.localStorage.setItem('activity', JSON.stringify(currentThings));
+    $('#actt').val('');
+  });
+
+  //FEELINGS
+  // quotationsImg, getItem and setItem,
+  $('body').on('click', '.feelingsImg', function() {
+    var thing = $('#feelt').val();
+    var currentThings = JSON.parse(window.localStorage.getItem('feelings')) || []
+    currentThings.push(thing)
+    window.localStorage.setItem('feelings', JSON.stringify(currentThings));
+    $('#feelt').val('');
+  });
+
+  //FILM
+  // quotationsImg, getItem and setItem,
+  $('body').on('click', '.filmImg', function() {
+    console.log('hello')
+    var thing = $('#filmt').val();
+    var currentThings = JSON.parse(window.localStorage.getItem('film')) || []
+    currentThings.push(thing)
+    window.localStorage.setItem('film', JSON.stringify(currentThings));
+    $('#filmt').val('');
+  });
+
+  //MUSIC
+  // quotationsImg, getItem and setItem,
+  $('body').on('click', '.musicImg', function() {
+    var thing = $('#musict').val();
+    var currentThings = JSON.parse(window.localStorage.getItem('music')) || []
+    currentThings.push(thing)
+    window.localStorage.setItem('music', JSON.stringify(currentThings));
+    $('#musict').val('');
+  });
+
+  //PERSON
+  // quotationsImg, getItem and setItem,
+  $('body').on('click', '.personImg', function() {
+    var thing = $('#pt').val();
+    var currentThings = JSON.parse(window.localStorage.getItem('person')) || []
+    currentThings.push(thing)
+    window.localStorage.setItem('person', JSON.stringify(currentThings));
+    $('#pt').val('');
+  });
+
+  //PLACE
+  // quotationsImg, getItem and setItem,
+  $('body').on('click', '.placeImg', function() {
+    var thing = $('#placet').val();
+    var currentThings = JSON.parse(window.localStorage.getItem('place')) || []
+    currentThings.push(thing)
+    window.localStorage.setItem('place', JSON.stringify(currentThings));
+    $('#placet').val('');
+  });
+
+  //QUOTATIONS
   // quotationsImg, getItem and setItem,
   $('body').on('click', '.quotationsImg', function() {
-    var thing = $('textarea').val();
-    debugger
+    var thing = $('#quot').val();
     var currentThings = JSON.parse(window.localStorage.getItem('quotations')) || []
     currentThings.push(thing)
     window.localStorage.setItem('quotations', JSON.stringify(currentThings));
-    $('input').val('');
-  })
-})
+    $('#quot').val('');
+  });
+});
